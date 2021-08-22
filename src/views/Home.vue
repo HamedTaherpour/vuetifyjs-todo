76e160bd86c78ad8b05c26e8cd244f706a76ad2c
Hamed Taherpour <hamedrazor54@gmail.com>
2021-08-22 19:03:51 +0430

    lesson-4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="home">
-    Home Page
+    <div class="display-1">
+      display-1
+    </div>
+    <div class="subtitle-2">
+      subtitle-1
+    </div>
+    <p class="red lighten-4 red--text">Vuetify is a Vue UI Library with beautifully handcrafted Material Components. No design skills required — everything you need to create amazing applications is at your fingertips.</p>
+    <span class="caption">caption</span>
+    <br>
+    <span class="overline">overline</span>
+    <p class="text-center font-weight-bold text-decoration-overline text-lowercase">
+      Vuetify is a Vue UI Library wit
+    </p>
   </div>
 </template>
 

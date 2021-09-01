1a2e91a8b766ced6c48ef9752dd1af1cc2591487
Hamed Taherpour <hamedrazor54@gmail.com>
2021-09-01 19:33:57 +0430

    lesson-7

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,4 @@
 <template>
-  <div class="text-center text-h1">
-    Home Page
-    <br>
-    <br>
-    <br>
-    <br>
-    <div>
-      <h1 class="d-flex d-sm-none">Phone</h1>
-      <h1 class="d-none d-sm-flex d-md-none">Tablet</h1>
-      <h1 class="d-none d-md-flex d-lg-none">Laptop</h1>
-      <h1 class="d-none d-lg-flex d-xl-none">Desktop</h1>
-      <h1 class="d-none d-xl-flex">4K</h1>
-    </div>
-  </div>
 </template>
 
 <script>

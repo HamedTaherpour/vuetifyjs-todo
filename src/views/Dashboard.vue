2f23fd27c9f559e4b24b5f2531e7aef02c6321b6
Hamed Taherpour <hamedrazor54@gmail.com>
2021-09-16 12:43:36 +0430

    lesson-14

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,19 +2,23 @@
   <div class="fill-height pa-5">
     <h2 class="subtitle-2 grey--text">Dashboard</h2>
     <v-container class="my-6">
-      <v-card>
-        <v-row justify="space-around" class="grey">
-          <v-col cols="1" class="red">
-            title
+      <v-card class="pa-3" v-for="project in projects" :key="project.title">
+        <v-row class="project" :class="project.status">
+          <v-col cols="12" md="6">
+            <div class="caption">Project title</div>
+            <div v-text="project.title">vue</div>
           </v-col>
-          <v-col cols="1" class="yellow">
-            name
+          <v-col cols="6" sm="4" md="2">
+            <div class="caption">Developer</div>
+            <div v-text="project.developer">vue</div>
           </v-col>
-          <v-col cols="1" class="green">
-            age
+          <v-col cols="6" sm="4" md="2">
+            <div class="caption">Date</div>
+            <div v-text="project.date">vue</div>
           </v-col>
-          <v-col cols="1" class="blue">
-            car
+          <v-col cols="12" sm="4" md="2">
+            <div class="caption">Status</div>
+            <div v-text="project.status">vue</div>
           </v-col>
         </v-row>
       </v-card>
@@ -24,10 +28,29 @@
 
 <script>
 export default {
-  name: "Dashboard"
+  name: "Dashboard",
+  data: () => ({
+    projects: [
+      {title: 'Design a new website', developer: 'Hamed Taherpour', date: '1st Jan 2019', status: 'doing'},
+      {title: 'Code up the homepage', developer: 'Chun Li', date: '10th Jan 2019', status: 'done'},
+      {title: 'Design video thumbnails', developer: 'Ryu', date: '20th Dec 2018', status: 'done'},
+      {title: 'Create a community forum', developer: 'Gouken', date: '20th Oct 2018', status: 'bugs'},
+    ]
+  })
 }
 </script>
 
 <style scoped>
+.project.done {
+  border-left: 4px solid #3CD1C2;
+}
+
+.project.doing {
+  border-left: 4px solid orange
+}
+
+.project.bugs {
+  border-left: 4px solid tomato;
+}
 
 </style>

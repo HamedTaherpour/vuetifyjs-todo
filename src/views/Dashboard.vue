b15965f0b1ac468e01144385923e362bbdc54567
Hamed Taherpour <hamedrazor54@gmail.com>
2021-09-16 11:52:57 +0430

    lesson-13

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,7 +2,22 @@
   <div class="fill-height pa-5">
     <h2 class="subtitle-2 grey--text">Dashboard</h2>
     <v-container class="my-6">
-      Update your layout without creating new classes. Spacing helpers are useful for modifying the padding and margin of an element.Try out an interactive screencast on how Vuetify spacing works.
+      <v-card>
+        <v-row justify="space-around" class="grey">
+          <v-col cols="1" class="red">
+            title
+          </v-col>
+          <v-col cols="1" class="yellow">
+            name
+          </v-col>
+          <v-col cols="1" class="green">
+            age
+          </v-col>
+          <v-col cols="1" class="blue">
+            car
+          </v-col>
+        </v-row>
+      </v-card>
     </v-container>
   </div>
 </template>

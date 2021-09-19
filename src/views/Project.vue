cdb3530f717e949dcf31e60ab0fad78b47eb82fd
Hamed Taherpour <hamedrazor54@gmail.com>
2021-09-19 19:34:28 +0430

    lesson-20

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -2,14 +2,54 @@
   <div class="fill-height pa-5">
     <h2 class="subtitle-2 grey--text">Project</h2>
     <v-container class="my-6">
-      Update your layout without creating new classes. Spacing helpers are useful for modifying the padding and margin of an element.Try out an interactive screencast on how Vuetify spacing works.
+      <v-expansion-panels accordion>
+        <v-expansion-panel v-for="project in projects" :key="project.title">
+          <v-expansion-panel-header>{{ project.title }}</v-expansion-panel-header>
+          <v-expansion-panel-content class="grey--text">
+            <div class="font-weight-bold">Date by: {{ project.date }}</div>
+            <div class="caption" v-text="project.content"></div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </v-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Project"
+  name: "Project",
+  data: () => ({
+    projects: [
+      {
+        title: 'Design a new website',
+        person: 'Hamed Taherpour',
+        date: '1st Jan 2019',
+        status: 'ongoing',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'
+      },
+      {
+        title: 'Code up the homepage',
+        person: 'John Leider',
+        date: '10th Jan 2019',
+        status: 'complete',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'
+      },
+      {
+        title: 'Design video thumbnails',
+        person: 'Maggie Appleton',
+        date: '20th Dec 2018',
+        status: 'complete',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'
+      },
+      {
+        title: 'Create a community forum',
+        person: 'Gouken',
+        date: '20th Oct 2018',
+        status: 'overdue',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'
+      },
+    ]
+  }),
 }
 </script>
 

7dca5dda8f78e8fa107f557170d09825835e1e10
Hamed Taherpour <hamedrazor54@gmail.com>
2021-09-27 13:17:58 +0330

    lesson-26

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -4,23 +4,38 @@
     <v-container class="my-6">
       <v-row>
         <v-col cols="12" sm="6" md="4" lg="3" v-for="person in team" :key="person.name">
-          <v-card outlined class="text-center">
-            <v-avatar class="mt-5" size="100">
-              <v-img
-                  :src="person.avatar"
-                  :alt="person.name"
-              ></v-img>
-            </v-avatar>
-            <v-card-title class="d-block" v-text="person.name"></v-card-title>
-            <v-card-subtitle v-text="person.role"></v-card-subtitle>
-            <v-card-text v-text="person.bio"></v-card-text>
-            <v-card-actions>
-              <v-btn outlined>
-                <v-icon small left>mdi-message</v-icon>
-                <span>Message</span>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
+          <v-hover v-slot:={hover}>
+            <v-card outlined class="text-center">
+              <v-avatar class="mt-5" size="100">
+                <v-img
+                    :src="person.avatar"
+                    :alt="person.name"
+                ></v-img>
+              </v-avatar>
+              <v-card-title class="d-block" v-text="person.name"></v-card-title>
+              <v-card-subtitle v-text="person.role"></v-card-subtitle>
+              <v-card-text>
+                <div class="caption" v-text="person.bio"></div>
+                <div class="flex flex-row mt-3" :class="hover ? 'show' : 'hide'">
+                  <v-btn icon small>
+                    <v-icon small>mdi-email-outline</v-icon>
+                  </v-btn>
+                  <v-btn icon small class="mx-3">
+                    <v-icon small>mdi-phone</v-icon>
+                  </v-btn>
+                  <v-btn icon small>
+                    <v-icon small>mdi-map</v-icon>
+                  </v-btn>
+                </div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn outlined>
+                  <v-icon small left>mdi-message</v-icon>
+                  <span>Message</span>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-hover>
         </v-col>
       </v-row>
     </v-container>
@@ -68,5 +83,11 @@ export default {
 </script>
 
 <style scoped>
+.hide {
+  opacity: 0;
+}
 
+.show {
+  opacity: 1;
+}
 </style>
